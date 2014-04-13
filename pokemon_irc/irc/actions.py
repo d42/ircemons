0d95a5c472f4ce8eba5e5a93bdfba973e72725b5
dzikie drożdże <daz@hackerspace.pl>
2014-04-13 02:10:57 +0200

    :3

--- pokemon_irc/irc/actions.py
+++ pokemon_irc/irc/actions.py
@@ -2,6 +2,7 @@ from pokemon_irc.models import orm
 from pokemon_irc.models.orm import session
 from pokemon_irc.game import events
 from collections import deque
+from datetime import datetime
 import logging
 
 
@@ -9,16 +10,21 @@ class GMActions:
 
 
     def __init__(self, GMBot):
-        self.public_actions = {
+        self.no_auth_actions = {
             'register': self._register_player,
             'auth': self._auth_player,
         }
+
+
+        self.public_actions = {
+            'help': self._display_help,
+        }
+        self.public_actions.update(self.no_auth_actions)
 
         self.main_actions = {
             'challenge': self._challenge_player,
             'accept': self._accept_challenge,
             'yield': self._yield_match,
-            'help': self._display_help,
         }
 
         self.battle_actions = {
@@ -33,7 +39,10 @@ class GMActions:
         self.query_actions = {
             'list': {'pokemons': self._list_pokemons}
         }
+
         self.query_actions.update(self.main_actions)
+        self.query_actions.update(self.public_actions)
+
         self.main_actions.update(self.public_actions)
 
         self.gm = GMBot
@@ -49,44 +58,30 @@ class GMActions:
         return inner
 
     @key_error_decorator
-    def query_run(self, user, command_tokens):
-        command_tokens = deque(command_tokens)
-
-        state = self.query_actions
-        while command_tokens:
-            state = state[command_tokens.popleft()]
-            if hasattr(state, '__call__'):
-                return state(*command_tokens, user=user)
-        return "something went wrong"
+    def query_run(self, user, tokens):
+        return self._run(user, tokens, self.query_actions)
 
     @key_error_decorator
-    def admin_run(self, command_tokens):
-        state = self.admin_actions
-        while command_tokens:
-            state = state[command_tokens.popleft()]
-            if hasattr(state, '__call__'):
-                return state(*command_tokens, player_name=nickserv_auth)
-        return False
+    def admin_run(self, tokens):
+        return self._run(user, tokens, self.admin_actions) # FIXME: this is borked
 
     @key_error_decorator
-    def battle_run(self, user, command_tokens):
-        state = self.battle_actions
-        while command_tokens:
-            state = state[command_tokens.popleft()]
-            if hasattr(state, '__call__'):
-                return state(*command_tokens, user=user)
-        return "something went wrong"
+    def battle_run(self, user, tokens):
+        return self._run(user, tokens, self.battle_actions)
 
     @key_error_decorator
-    def main_channel_run(self, user, command_tokens):
-        command_tokens = deque(command_tokens)
+    def main_channel_run(self, user, tokens):
+        return self._run(user, tokens, self.main_actions)
 
-        state = self.main_actions
-        while command_tokens:
-            state = state[command_tokens.popleft()]
+    def _run(self, user, tokens, commands):
+        state = commands
+        while tokens:
+            state = state[tokens.popleft()]
             if hasattr(state, '__call__'):
-                return state(*command_tokens, user=user)
-        return "something went wrong"
+                return state(*tokens, user=user)
+        return "Something went wrong"
+
+    ##### Event wrappers
 
     def _list_pokemons(self, user):
         return events.list_pokemons(user.auth)
@@ -95,25 +90,25 @@ class GMActions:
         return events.create_player(user.nick, password)
 
     def _auth_player(self, password, user):
-        return events.authorize_player(user.nick, password)
+        message = events.authorize_player(user.nick, password)
 
+        if message == "authorized successfully":
+            logging.debug(user, "authorized")
+            self.gm.authorized[user.nick] = user.hostname
+        return message
 
     def _print(self, *message, user):
         return ' '.join(message)
 
     def _challenge_player(self, challengee, user):
-        if not self.gm.player_on_main_channel(challengee): return "You can't challenge someone who's not on the channel."
-
-
-
+        if not self.gm.player_on_main_channel(challengee): return "no_such_player"
         self.gm.write(challengee, "{} has challened you to a battle".format(user))
+        return "{nick} has been challenged".format(nick=challengee)
 
-        return "{} has been challenged".format(challengee)
-
-    def _recall_pokemon(name, user, battle):
+    def _recall_pokemon(name, battle, user):
         pass
 
-    def _summon_pokemon(name, user, battle):
+    def _summon_pokemon(name, battle, user):
         pass
 
     def _accept_challenge(self, challenger=None, user=None):
--- pokemon_irc/irc/irc_client.py
+++ pokemon_irc/irc/irc_client.py
@@ -3,24 +3,14 @@ import irc.strings
 import logging
 from irc.client import ip_numstr_to_quad, ip_quad_to_numstr
 from pokemon_irc.settings import IRC_SERVER, IRC_PORT, IRC_MAIN_CHANNEL, IRC_GM_NICK, IRC_REALNAME, IRC_OWNER
-from pokemon_irc.game.actions import GMActions
+from .actions import GMActions
 from collections import deque, defaultdict, namedtuple
 
 user = namedtuple('user', ['nick', 'real_name', 'hostname'])
+user_auth = namedtuple('auth', ['hostname', 'player'])
 
-
-class Authorization:
-
-    def check_auth(self, user_name):
-        return user_name
-
-
-
-# handle 330
 
 class PokeBot(irc.bot.SingleServerIRCBot):
-    auth = Authorization()
-
     def parse_source(self, source):
         t = str.maketrans('!@', '  ')
         u = user(*source.translate(t).split())
@@ -31,14 +21,18 @@ class PokeBot(irc.bot.SingleServerIRCBot):
         """ turn into valid irc nickname """
         pass
 
-    def parse_command(self, message, name=None):
+    def tokenize_command(self, message, name=None, query=False):
         if not name: name = self.name
+        command = None
 
         if message.startswith(self.name+':'):
             command = message[len(self.name)+1:].strip()
+        elif query:
+            command = message.strip()
+
+        if command:
             logging.debug(command)
             tokens = deque(command.split())
-
             return tokens
         return False
     
@@ -60,26 +54,14 @@ class PokemonBot(PokeBot):
         c.join(self.channel)
 
     def on_privmsg(self, c, e):
-        nick = self.nick(e.source)
-        print(nick)
-        self.connection.privmsg('nickserv', 'info %s' % nick)
-        self.do_command(e, e.arguments[0])
+        pass
 
     def on_pubmsg(self, c, e):
         pass
-        #a = e.arguments[0].split(":", 1)
-        #if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(self.connection.get_nickname()):
-            #self.do_command(e, a[1].strip())
-        #return
-
-    #def do_command(self, e, cmd):
-        #nick = e.source.nick
-        #c = self.connection
 
 
 
 class GMBot(PokeBot):
-    auth_queue = deque()
     authorized = {}
     pending_battles = defaultdict(dict)
     current_battles = defaultdict(dict)
@@ -98,43 +80,84 @@ class GMBot(PokeBot):
         self.channel = channel
         self.actions = GMActions(self)
 
+        self.connection.add_global_handler("part", self.on_exit, -42)
+        self.connection.add_global_handler("quit", self.on_exit, -42)
+        self.connection.add_global_handler("nick", self.move_auth, -42)
+        self.connection.add_global_handler("welcome", self.clear_auth, 1)
+
+    def clear_auth(self, c, e):
+        self.authorized = {}
+
+    def move_auth(self, c, e):
+        user = self.parse_source(c.source)
+        if user.name not in self.authorized: return
+        if user.hostname == self.authorized[user.name].hostname:
+            self.authorized[e.target] = self.authorized[user.name]
+            del self.authorized[user.name]
+
+    def auth(self, user, player):
+        logging.debug(user, "authorized")
+        self.gm.authorized[user.nick] = user_auth(user.hostname, player)
+
+    def deauth(self, user):
+        auth = self.authorized.get(user.nick, None)
+        if not auth: return
+
+        if auth.hostname == user.hostname:
+            del self.auth_user[user.nick]
+        assert not auth  # shouldn't happen, amirite?
+
+    def on_exit(self, c, e):
+        user = self.parse_source(c.source)
+
+        if e.type == 'quit': self.deauth(user)
+        elif e.target == IRC_MAIN_CHANNEL: self.deauth(user)
+
     def on_nicknameinuse(self, c, e):
-        c.nick(c.get_nickname() + "_")
+        raise("get me a real name!")  # TODO: get me a real exception :v
 
     def on_welcome(self, c, e):
         c.join(self.channel)
 
-    def on_privnotice(self, c, e):
-        print(e.arguments)
+    # TODO: refactoring this with on_pubmsg would be cool
+    def on_privmsg(self, c, e):  # TODO: refactoring this with on_pubmsg would be cool
 
+        user = self.parse_source(e.source)
+        tokens = self.tokenize_command(e.arguments[0], query=True)
 
-    def on_privmsg(self, c, e):
-        nick = get_nick(e.source)
-        tokens = deque(e.arguments[0].split())
-        nickserv_auth = self.auth.check_auth(nick)
+        if not tokens: return
 
-        if not nickserv_auth:
-            self.respond(nick, e.target, "auth to nickserv plx")
+        hostname = self.authorized.get(user.nick, False)
+        player = user.hostname == self.hostname
+
+        if player and tokens[0] in self.actions.no_auth_actions:
+            self.write(user.nick, "already authorized")
             return
-        message = self.actions.query_run(nickserv_auth, tokens)
 
-        self.write(nick, message)
+        if not player and tokens[0] not in self.actions.public_actions:
+            self.write(user.nick, "Not authorized")
+            return
+
+        message = self.actions.query_run(user, tokens)
+        self.write(user.nick, message)
 
 
-    def player_on_main_channel(self, nick):
-        return nick in self.channels[IRC_MAIN_CHANNEL].users()
+    #def player_on_main_channel(self, nick): # such obsolete wow
+        #import ipdb; ipdb.set_trace()
+        #return nick in self.channels[IRC_MAIN_CHANNEL].users()
 
 
     def on_pubmsg(self, c, e):
         user = self.parse_source(e.source)
-        tokens = self.parse_command(e.arguments[0])
+        tokens = self.tokenize_command(e.arguments[0])
 
         if not tokens: return
 
         player = self.authorized.get((user.nick, user.hostname), False)
 
-        if player:
-            user.auth = player
+        if player and tokens[0] in self.actions.no_auth_actions:
+            self.respond(user.nick, e.target, "already authorized")
+            return
 
         if not player and tokens[0] not in self.actions.public_actions:
             self.respond(user.nick, e.target, "not authorized")
@@ -148,7 +171,8 @@ class GMBot(PokeBot):
         self.respond(user.nick, e.target, message)
 
     def run(self, command, channel=None):
-        """ kind of motherfucking admin console """
+        # pointless, probably bad, but i found it interesting
+
         name = "motherfucking_admin"
         tokens = deque(command.split())
         if not channel:
@@ -163,24 +187,15 @@ class GMBot(PokeBot):
         self.write(channel, message)
 
     def write(self, channel, message):
+        """ send message to a channel(username) """
         for line in message.split('\n'):
             print("#%s %s: %s" % (channel, IRC_GM_NICK, line))
             self.connection.privmsg(channel, line)
 
     def respond(self, nick, channel, message):
+        """ send message to a channel, prefixed with nick: """
         print("#%s %s: %s" % (channel, nick, message))
         self.connection.privmsg(channel, "%s: %s" % (nick, message))
-
-    def _ask_nickserv(self, nickname):
-        pass
-
-    def on_nickserv_reply(self, nickname, verified):
-        pass
-
-
-    def nick(self, source):
-        return source.split('!')[0]
-
 
     #def create_pokemon(self, create_func):
         #self.create_func(self, "pikachu")
