612061ebfdf8b5250f8cba32708f822dce654373
dzikie drożdże <daz@hackerspace.pl>
2014-04-28 01:36:37 +0200

    derpderp

--- pokemon_irc/web/pokedb.py
+++ pokemon_irc/web/pokedb.py
@@ -18,35 +18,11 @@ class PokeDBMachine:
         self.session = requests.session()
         self.cache = {}
 
-    def get_columns(self, row, datatype):
-        ret_list = []
-        #specials = {'½': 0.5, '2': 2, '0': 0, '∞': INF, '-': MINUS}
+    def get_columns(self, row):
 
-        def get(val, def_val):
-            if not def_val:
-                return val
 
-            if not val:
-                return def_val()
-
-            val0 = val[0].strip()
-            if def_val == str:
-                return val0
-
-            if def_val == list:
-                return [val0] if len(val) == 1 else val
-
-            if def_val == int:
-                return int(SPECIALS.get(val0, val0))
-
-            if def_val == float:
-                return float(SPECIALS.get(val0, val0))
-
-            return def_val(val)
-
-        for (col, val) in zip_longest(row, datatype.defaults):
-            ret_list.append(get(col.xpath('.//text()'), val))
-        return ret_list
+            return [get(val.xpath('.//text()'))
+        for val in row]
 
     def request_table(self, url, xpath):
 
@@ -61,38 +37,38 @@ class PokeDBMachine:
         table = tree.xpath(xpath)
         return table
 
-    def poke_get(self, datatype, **kwargs):
-        url = datatype.url.format(**kwargs)
+    def poke_get(self, xpath, url, **kwargs):
+        url = url.format(**kwargs)
 
-        xpath = datatype.xpath
+        xpath = xpath
         table = self.request_table(url, xpath)
 
         def parse_row(row):
-            return self.get_columns(row, datatype)
+            return self.get_columns(row)
 
         for row in table:
-            yield datatype(*parse_row(row))
+            yield parse_row(row)
+            #yield (parse_row(x for x in row)
 
     def poke_get_type_damage(self, datatype, **kwargs):
         url = datatype.url.format(**kwargs)
         xpath = datatype.xpath
 
         table = self.request_table(url, xpath)
+        table_size = len(table)
 
         def parse_row(row):
-            return self.get_columns(row, datatype)
+            return self.get_columns(row, [float] * table_size)
 
         types = [row[0].text_content().lower() for row in table]
         matrix = [parse_row(row[1:]) for row in table]
         damage = defaultdict(dict)
 
         for (i, j) in product(range(len(matrix)), repeat=2):
-            import ipdb; ipdb.set_trace()
             val = matrix[i][j]
             if val == 1 or not val:
                 continue
 
-            val = val[0]
             val = SPECIALS.get(val, val)
 
             typei, typej = types[i], types[j]
